33bdc3677d5d17bf9a460a99fd503454b855df78
Shemal Grey <shemalgrey@gmail.com>
2023-03-01 20:59:35 -0500

    Code update

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -15,39 +15,86 @@ export function displayBookList(req, res, next) {
 
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
-
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+// This will render the book details page with a blank form and passes the apporiate values for names an propert and blank values property 
+    router.get('/add', (req, res) => {
+        res.render('book-details', {
+          name: 'Add Book',
+          books: {}
+        });
+      });
 }
 
 // POST process the Book Details page and create a new Book - CREATE
+// Uses values from the name attirbutes from each field in the book detail page , the user also gets redirected back to the booklist page when done.
 export function processAddPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    router.post('/add', (req, res) => {
+        let book = new Book({
+          name: req.body.name,
+          author: req.body.author,
+          published: req.body.published,
+          description: req.body. description,
+          price: req.body.price
+        });
+        book.save((err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect('/books');
+          }
+        });
+      });
 }
 
 // GET the Book Details page in order to edit an existing Book
+//uses the id from the URL to select the book to be updated
 export function displayEditPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    router.get('/:id', (req, res) => {
+        let id = req.params.id;
+        Book.findById(id, (err, book) => {
+          res.render('book-details', {
+            title: 'Edit Book',
+            books: book
+          });
+        });
+      });
 }
 
 // POST - process the information passed from the details form and update the document
+//to edit an existing book in the database. The user will then be redirected back to the BookList page
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    router.post('/:id', (req, res) => {
+        let id = req.params.id;
+        let book = {};
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.published = req.body.published;
+        book.description = req.body.description;
+        book.price = req.body.price;
+      
+        let query = {_id: id};
+        Book.update(query, book, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect('/books');
+          }
+        });
+      });
 }
 
-// GET - process the delete by user id
+// GET - process the delete by user id 
+//and pass the id to the book model's remove method to delete the book from the database. 
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    router.get('/delete/:id', (req, res) => {
+        let id = req.params.id;
+        Book.remove({_id: id}, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect('/books');
+          }
+        });
+      });
 }
